a33d4b1fe55960e0b55f9a22a5da19887aea472b
Eric Schrijver <eric@ericschrijver.nl>
2014-10-06 13:06:57 +0200

    Explicative comments

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,22 @@
 var whereAreWe = function() {
+    // Ask the device for the position;
+    // When we found it run the function ‘updatePosition’ (a ‘callback’)
+    
    navigator.geolocation.getCurrentPosition(updatePosition);
 }
 
 var updatePosition = function(position) {
-    var locationElement = document.querySelector('#location');
+    // Get the element with the id ‘location’
+    // In this element, put everything we know about the coordinates of the position
     
+    var locationElement = document.querySelector('#location');
+
     locationElement.innerHTML = JSON.stringify(position.coords, null, 2);
 }
 var initialize = function() {
     // When the event `deviceready` is triggered,
     // (when the Cordova functions have iniated)
     // run the function `showReady`
+    
     document.addEventListener('deviceready', whereAreWe, false);
 }
