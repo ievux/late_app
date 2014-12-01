5fafc79a8d399934c8d90292a5db005f848c3c81
Eric Schrijver <eric@ericschrijver.nl>
2014-12-01 02:46:39 +0100

    Find the current country—and tell us it rains more often in Colombia

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,29 @@
+// in country_reverse_geocoding.js we included a function, that on the basis
+// of a latitude and longitude attempts to find out in what country we are
+var rgc = new country_reverse_geocoding();
+
 var whereAreWe = function() {
-    // Ask the device for the position;
-    // When we found it run the function ‘updatePosition’ (a ‘callback’)
-    
-   navigator.geolocation.getCurrentPosition(updatePosition);
+    // Ask the device for the current position;
+    // When we find it run the function ‘updatePosition’ (a ‘callback’)
+    navigator.geolocation.getCurrentPosition(updatePosition, onError);
 }
 
 var updatePosition = function(position) {
-    // Get the element with the id ‘location’
-    // In this element, put everything we know about the coordinates of the position
-    
-    var locationElement = document.querySelector('#location');
+    var country = rgc.get_country(position.coords.latitude, position.coords.longitude);
+    var $results = $('#results');
 
-    locationElement.innerHTML = JSON.stringify(position.coords, null, 2);
+    $results.html( $results.html().replace("{{ country }}", country.name) );
+    
+    $("#loading").addClass("hidden");
+    $results.removeClass("hidden");
 }
+
+var onError = function(error) {
+    // this code is run if the app fails in finding out where we are
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+};
+
 var initialize = function() {
     // When the event `deviceready` is triggered,
     // (when the Cordova functions have iniated)
